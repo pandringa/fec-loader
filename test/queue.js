7506ce8c0a366a504a27d2efd083085ebfb56760
Chris Zubak-Skees <chriszs@gmail.com>
2017-08-20 19:43:02 -0400

    hmm

--- test/queue.js
+++ test/queue.js
@@ -8,26 +8,20 @@ const ONE_SECOND = 1000;
 let should = chai.should();
 
 describe('lib/queue.js', () => {
-    let queue;
-
-    beforeEach(function() {
-        this.clock = sinon.useFakeTimers();
-    });
-
-    afterEach(function() {
-        this.clock.restore();
-    });
 
     it('should repeat every 2 seconds', function(done) {
-        queue = new Queue('test');
+        this.clock = sinon.useFakeTimers();
+
+        let queue = new Queue('test');
 
         queue.process((task,done) => {
             done();
         });
 
-        let _this = this;
+        let self = this;
+
         let date = new Date('2017-02-07 9:24:00');
-        this.clock.tick(date.getTime());
+        self.clock.tick(date.getTime());
         let nextTick = 2 * ONE_SECOND + 500;
 
         queue
@@ -36,13 +30,13 @@ describe('lib/queue.js', () => {
                 { repeat: { cron: '*/2 * * * * *' } }
             )
             .then(() => {
-                _this.clock.tick(nextTick);
+                self.clock.tick(nextTick);
             });
 
         let prev;
         let counter = 0;
         queue.on('completed', function(job) {
-            _this.clock.tick(nextTick);
+            self.clock.tick(nextTick);
             if (prev) {
                 prev.timestamp.should.be.lt(job.timestamp);
                 (job.timestamp - prev.timestamp).should.be.gte(2000);
@@ -51,7 +45,9 @@ describe('lib/queue.js', () => {
             counter++;
             if (counter == 20) {
                 queue.close()
-                    .then(done);
+                    .then(() => {
+                        self.clock.restore();
+                    });
             }
         });
     });
